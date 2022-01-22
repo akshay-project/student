114e21f8719f12da1f8fb292bbe3620706939cc5
akshay-project <83492095+akshay-project@users.noreply.github.com>
2022-01-22 19:38:02 +0530

    Updates

---
+++ precache-manifest.c8524f0f599aa74dd38cbd16ec9059d9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb2f4fb219715bfd6141f06c8dd3a5ed",
+    "url": "/student/index.html"
+  },
+  {
+    "revision": "bd018c141bf60611e50c",
+    "url": "/student/static/js/2.c960bc5b.chunk.js"
+  },
+  {
+    "revision": "0a554b8f698daab4dced",
+    "url": "/student/static/js/main.1ae73a2e.chunk.js"
+  },
+  {
+    "revision": "10e8500c29d2e5b7451f",
+    "url": "/student/static/js/runtime~main.585d643e.js"
+  }
+]);
